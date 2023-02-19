246f85976e9af5d3f61d13c5f9a1d6faf8b497d2
Gereon Fox <ger.fox07@t-online.de>
2023-02-19 20:03:04 +0100

    Turned serve.py into a stub for what it is to become.

--- serve.py
+++ serve.py
@@ -11,214 +11,45 @@ from trigs.error import TrigsError
 from trigs.players.pyaudio import PyAudioPlayer, PlayerStatus
 from trigs.playlist import resolve_playlist, load_wav
 from trigs.remote.player import RemotePlayer
-from trigs.remote.protocol import PlayerClient
+from trigs.remote.protocol import PlayerServer
 from trigs.remote.tcp import TCPConnection
 from trigs.triggers.bluetooth import BluetoothTrigger, TriggerError
 from trigs.triggers.virtual import VirtualTriggerWindow
 
 # region Argument parsing
 
-parser = argparse.ArgumentParser(description='Processes the input events from trigger devices to control a playlist'
-                                             'of media files.')
+parser = argparse.ArgumentParser(description='Serves as an audio player to which remote clients'
+                                             ' can connect and control playback.')
 
-parser.add_argument('playlist', type=str, help='The path to the playlist file that is to be controlled.')
+parser.add_argument('hostname', type=str, help='The host name for which this server should accept connections.')
+parser.add_argument('port', type=int, help='The port on which this server should listen for connections.')
 
-parser.add_argument('--virtual', action='store_true', default=False,
-                    help='Instead of expecting bluetooth devices to be connected, show an'
-                         'array of virtual triggers that can be activated by mouse click or'
-                         'key press.')
-
-parser.add_argument('--remote', type=str, nargs=2, help='Instead of launching a local audio player, this will make'
-                                                        'the process connect to a trigs server on a remote machine.'
-                                                        'You need to give the host name and port number for that machine!')
 
 # endregion
-
-
-async def calibrate(display=None, forward_uniq=None, backward_uniq=None):
-    """
-    Waits for two trigger devices to be connected and asks the user to indicate their roles.
-    :param display: The Display object that should be used issue signals during the calibration process.
-    :param forward_uniq: The unique and persistent identifier of the device that should be used for the 'forward' trigger.
-                         If this is given and connected, the user won't be asked to actively participate in calibration.
-    :param backward_uniq: The unique and persistent identifier of the device that should be used for the 'backward' trigger.
-                         If this is given and connected, the user won't be asked to actively participate in calibration.
-    :return: A pair (forward, backward) of Trigger objects.
-    """
-
-    while True:
-        triggers = []
-        print("Waiting for at least 2 trigger devices to be connected...")
-
-        if display is not None:
-            display.set_color(0, (0, 0, 255))
-            display.set_color(1, (0, 0, 255))
-
-        while len(triggers) < 2:
-            # Close previously discovered trigger objects, because they still might have devices grabbed that 'discover'
-            # would try to grab again:
-            for t in triggers:
-                t.close()
-            triggers.clear()
-
-            try:
-                triggers = list(BluetoothTrigger.discover())
-            except TriggerError:
-                print("FAILED TO DISCOVER TRIGGERS. Trying again...")
-                pass
-
-            await asyncio.sleep(0.2)
-
-        print("Triggers connected!")
-
-        uniq2trig = {t.uniq: t for t in triggers}
-
-        while True:
-
-            if display is not None:
-                display.set_color(0, (0, 0, 255))
-                display.set_color(0, (0, 0, 255))
-
-            try:
-                forward = uniq2trig[forward_uniq]
-            except KeyError:
-                try:
-                    print("\tPlease trigger 'forward' once!")
-                    forward = (await first((t.next() for t in triggers))).source
-                    print("\tForward triggered.")
-                except TriggerError:
-                    print("LOST CONNECTION TO A TRIGGER DURING CALIBRATION!")
-                    break
-
-            if display is not None:
-                display.set_color(1, (255, 255, 255))
-
-            try:
-                backward = uniq2trig[backward_uniq]
-            except KeyError:
-                try:
-                    print("\tPlease trigger 'backward' once!")
-                    backward = (await first((t.next() for t in triggers))).source
-                    print("\tBackward triggered.")
-                except TriggerError:
-                    print("LOST CONNECTION TO A TRIGGER DURING CALIBRATION!")
-                    break
-
-            if display is not None:
-                display.set_color(0, (255, 255, 255))
-
-            if forward is backward:
-                print("Cannot use the same trigger for forward and backward! Please try again!")
-                continue
-
-            print("CALIBRATION COMPLETE.")
-            return forward, backward
 
 
 async def main():
 
     args = parser.parse_args()
 
-    def on_window_closed():
-        for t in asyncio.all_tasks():
-            if t is not asyncio.current_task():
-                t.cancel()
-
-    window = None
-    player = None
-    connection = None
+    listener = None
 
     try:
+        print("Serving for {}:{}...".format(args.host, args.port))
+        server = PlayerServer()
+        listener = asyncio.create_task(TCPConnection.serve(args.host, args.port, server.serve_client))
 
-        if args.remote is None:
-            player = PyAudioPlayer(paths=[args.playlist])
-        else:
-            host, port = args.remote
-            begin("Connecting to {}:{}", host, port)
-            connection = TCPConnection.open_outgoing(host, int(port))
-            done()
-            player = RemotePlayer(PlayerClient(connection))
-            begin("Initializing remote playlist")
-            await player.clear_sequences()
-            for path in resolve_playlist([args.playlist]):
-                w, c, r, data = load_wav(path)
-                await player.append_sequence(w, c, r, data)
-            done()
+        with PyAudioPlayer(paths=[]) as player:
 
-        if args.virtual:
-            window = VirtualTriggerWindow([("Stop (Key: s)", "s"), ("Next (Key: k)", "k")], on_close=on_window_closed)
-        else:
-            window = Display(2, on_close=on_window_closed)
+            while True:
+                request = await server.next_request()
 
-        if args.virtual:
-            backward, forward = window.triggers
-        else:
-            forward, backward = await calibrate(display=window)
+                print("Got a request :-)")
+                return
 
-        while True:
-
-            try:
-                event = await first((forward.next(), backward.next()))
-            except TriggerError:
-                if args.virtual:
-                    raise
-                else:
-                    print("LOST CONNECTION TO AT LEAST ONE TRIGGER!")
-                    # Close the triggers, to make sure none of them remain grabbed:
-                    fu = forward.uniq
-                    bu = backward.uniq
-                    forward.close()
-                    backward.close()
-                    del forward, backward
-                    forward, backward = await calibrate(display=window, forward_uniq=fu, backward_uniq=bu)
-                    continue
-
-            if event.source is forward:
-                # If this happens while a sequence is still underway, ignore it.
-                if await player.status == PlayerStatus.PLAYING:
-                    print("IGNORED FORWARD, because sequence still playing!")
-                    continue
-                # Begin with the next sequence:
-                await player.play()
-                await player.play()  # Necessary because of the weird semantics VLC/playerctl give to 'stop'.
-
-                if not args.virtual:
-                    d = await player.duration
-                    window.flash(0, (0, 255, 0), duration=d)
-                    window.flash(1, (0, 255, 0), duration=d)
-
-                print("FORWARD!")
-            elif event.source is backward:
-                # If this happens while we are NOT playing a sequence, it probably happens while we are paused at
-                # the end of a sequence we just finished. In that case we certainly do not want to jump back to the
-                # beginning:
-                if await player.status != PlayerStatus.PLAYING:
-                    print("IGNORED BACKWARD, because we are already stopped!")
-                    continue
-                # The previous FORWARD was a mistake and should be undone. Since the only FORWARDs that ever take
-                # effect are those that we receive while we are paused in-between sequences, we just have to stop
-                # playback:
-                await player.stop()
-
-                if not args.virtual:
-                    window.flash(0, (255, 0, 0))
-                    window.flash(1, (255, 0, 0))
-
-                print("UNDO!")
-            else:
-                print("UNKNOWN EVENT:", event)
-
-    except TrigsError as te:
-        print(str(te))
-    except asyncio.exceptions.CancelledError:
-        print("Exiting.")
     finally:
-        if window is not None:
-            window.close()
-        if player is not None:
-            await player.terminate()
-        if connection is not None:
-            connection.close()
+        if listener is not None:
+            listener.cancel()
 
 
 if __name__ == '__main__':
